f10c76d340d5f588ce1f715056fd80cc5ff25f6e
IgorNewCodex1 <igor_ss@estudante.sc.senai.br>
2019-02-12 21:03:27 -0200

    projeto inicio exercicio

--- tsbasico-aula/src/perguntas.ts
+++ tsbasico-aula/src/perguntas.ts
@@ -8,6 +8,33 @@ export class Perguntas {
                     name: 'name',
                     type: 'input',
                     message: 'qual o seu nome',
+                },
+                {
+                    name: 'name',
+                    type: 'input',
+                    message: 'Qual o seu telefone',
+                },
+                {
+                    name: 'car',
+                        type: 'list',
+                        message: 'Qual sabor você gostaria?',
+                        choices: ['Clabresa', 'Frango', '4 queijos', 'Coração', 'Portuguesa'],
+                        default: 3,
+                },
+                {
+                    name: 'name',
+                    type: 'input',
+                    message: 'Qual o tamanho da pizza? você pode escolher entre\n Pequena \nMédia \nGrande ', 
+                },
+                {
+                    name: 'name',
+                    type: 'input',
+                    message: 'Quantas pizzas ?',
+                },
+                {
+                    name: 'name',
+                    type: 'input',
+                    message: 'Gostaria que nós entregássemos?',
                 }
             ]
         ).then(
